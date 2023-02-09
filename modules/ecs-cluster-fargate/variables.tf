01d055415ea13d13d37d21f83f67f50b272036c0
Ilya Melnik <ilya.melnik.svc@gmail.com>
2023-02-09 21:59:31 +0400

    Added variable for ecs module

--- modules/ecs-cluster-fargate/variables.tf
+++ modules/ecs-cluster-fargate/variables.tf
@@ -9,6 +9,11 @@ variable "container_insights" {
   default     = false
 }
 
+variable "kms_key_id" {
+  type        = string
+  description = "KMS key ID to use for encrypting resources."
+}
+
 variable "tags" {
   type        = map(string)
   description = "A map of tags to add to all resources created by this module."
